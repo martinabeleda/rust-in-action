f0f9df8055ea244d3f3250984603fef82c41a9b2
martinabeleda <martin.abeleda@gmail.com>
2023-04-16 20:34:49 -0700

    Cargo fmt ch3

--- ch3-files/src/main.rs
+++ ch3-files/src/main.rs
@@ -7,56 +7,56 @@ use std::fmt::Display;
 
 #[derive(Debug, PartialEq)]
 enum FileState {
-  Open,
-  Closed,
+    Open,
+    Closed,
 }
 
 /// Represents a "file",
 /// which probably lives on a file system.
 #[derive(Debug)]
 struct File {
-  name: String,
-  data: Vec<u8>,
-  state: FileState,
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
 }
 
 impl Display for FileState {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match *self {
-      FileState::Open => write!(f, "OPEN"),
-      FileState::Closed => write!(f, "CLOSED"),
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
     }
-  }
 }
 
 impl Display for File {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "<{} ({})>", self.name, self.state)
-  }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
 }
 
 impl File {
-  /// New files are assumed to be empty, but a name is required.
-  fn new(name: &str) -> File {
-    File {
-      name: String::from(name),
-      data: Vec::new(),
-      state: FileState::Closed,
+    /// New files are assumed to be empty, but a name is required.
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
     }
-  }
-  /// Returns the file's length in bytes.
-  pub fn len(&self) -> usize {
-    self.data.len()
-  }
+    /// Returns the file's length in bytes.
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
 
-  /// Returns the file's name.
-  pub fn name(&self) -> String {
-    self.name.clone()
-  }
+    /// Returns the file's name.
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
 }
 
 fn main() {
-  let f6 = File::new("f6.txt");
-  println!("{:?}", f6);
-  println!("{}", f6);
+    let f6 = File::new("f6.txt");
+    println!("{:?}", f6);
+    println!("{}", f6);
 }
