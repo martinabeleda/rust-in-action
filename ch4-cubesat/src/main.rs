3424012e9f863c82de47f35b1366c779a1ccefba
martinabeleda <martin.abeleda@gmail.com>
2023-04-16 20:34:03 -0700

    Add cubesat example

---
+++ ch4-cubesat/src/main.rs
@@ -0,0 +1,90 @@
+#![allow(dead_code, unused_variables)]
+
+#[derive(Debug)]
+struct CubeSat {
+  id: u64,
+}
+
+impl CubeSat {
+  fn recv(&mut self, mailbox: &mut Mailbox) -> Option<Message> {
+    mailbox.messages.pop()
+  }
+}
+
+#[derive(Debug)]
+enum StatusMessage {
+  Ok,
+}
+
+#[derive(Debug)]
+struct Mailbox {
+  messages: Vec<Message>,
+}
+
+impl Mailbox {
+  fn post(&mut self, msg: Message) {
+    self.messages.push(msg);
+  }
+
+  fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+    for i in 0..self.messages.len() {
+      if self.messages[i].to == recipient.id {
+        let msg = self.messages.remove(i);
+        return Some(msg);
+      }
+    }
+
+    None
+  }
+}
+
+#[derive(Debug)]
+struct Message {
+  to: u64,
+  content: String,
+}
+
+struct GroundStation {}
+
+impl GroundStation {
+  fn connect(&self, sat_id: u64) -> CubeSat {
+    CubeSat { id: sat_id }
+  }
+
+  fn send(&self, mailbox: &mut Mailbox, msg: Message) {
+    mailbox.messages.push(msg);
+  }
+}
+
+fn check_status(sat: &CubeSat) -> StatusMessage {
+  StatusMessage::Ok
+}
+
+fn fetch_sat_ids() -> Vec<u64> {
+  vec![0, 1, 2, 3]
+}
+
+fn main() {
+  let mut mail = Mailbox { messages: vec![] };
+
+  let base = GroundStation {};
+
+  let sat_ids = fetch_sat_ids();
+  for sat_id in sat_ids {
+    let sat = base.connect(sat_id);
+    let msg = Message {
+      to: sat_id,
+      content: String::from("hello"),
+    };
+
+    base.send(&mut mail, msg);
+  }
+
+  let sat_ids = fetch_sat_ids();
+  for sat_id in sat_ids {
+    let mut sat = base.connect(sat_id);
+
+    let msg = sat.recv(&mut mail);
+    println!("{:?} {:?}", sat, msg);
+  }
+}
